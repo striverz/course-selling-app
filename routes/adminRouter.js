6c1ef7ba17254990a18011c58a3945eff906d067
striverz <striverzarc@gmail.com>
2025-04-22 20:01:08 +0530

    created the functionality map of what functionality to be added to the application

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -34,7 +34,7 @@ adminRouter.post("/course", (req, res) => {
     });
   }
 });
-adminRouter.put("/course", (req, res) => {
+adminRouter.patch("/course", (req, res) => {
   try {
     res.json({
       message: "admin updates courses",
