9570416e2af06e4240e432b8f6c1e30be8a38141
striverz <striverzarc@gmail.com>
2025-04-23 13:10:33 +0530

    added the all end points for the user to purchase

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const courseRouter = express.Router();
 const { authUser } = require("../middlewares/authUser");
+const { courseModel, CourseModel } = require("../models/courseModel");
+const { PurchaseModel } = require("../models/purchaseModel");
 
-courseRouter.post("/purchase", authUser, (req, res) => {
+courseRouter.post("/purchase", authUser, async (req, res) => {
   try {
+    const userId = req.userId;
+    const { courseId } = req.body;
+
+    await PurchaseModel.create({
+      userId,
+      courseId,
+    });
+
     res.json({
-      message: "Cohort-3",
+      message: "The Course Purchase is successful",
     });
   } catch (err) {
     res.status(401).json({
@@ -14,22 +24,44 @@ courseRouter.post("/purchase", authUser, (req, res) => {
   }
 });
 
-courseRouter.get("/preview", authUser, (req, res) => {
+courseRouter.get("/purchase", authUser, async (req, res) => {
   try {
+    const userId = req.userId;
+    const purchasedCourses = await PurchaseModel.find({
+      userId: userId,
+    });
+
+    const purchaseCourseIds = [];
+    for (let i = 0; i < purchasedCourses.length; i++) {
+      purchaseCourseIds.push(purchasedCourses[i].courseId);
+    }
+
+    console.log(purchaseCourseIds);
+
+    const finallyPurchasedCourses = await CourseModel.find({
+      _id: { $in: purchaseCourseIds },
+    });
+
     res.json({
-      message: "100xDevs",
+      message: "All the purchased courses are",
+      finallyPurchasedCourses,
     });
   } catch (err) {
-    res.json({
+    res.status(401).json({
       message: err.message,
     });
   }
 });
-
-courseRouter.get("/purchase", authUser, (req, res) => {
+courseRouter.get("/preview", async (req, res) => {
   try {
+    const allCourseAvailable = await CourseModel.find({});
+
+    res.json({
+      message: "The all availabe courses are",
+      allCourseAvailable,
+    });
   } catch (err) {
-    res.status(401).json({
+    res.json({
       message: err.message,
     });
   }
